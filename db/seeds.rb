9f506733ed5275e1b854ca6b8c3d3d63f3fb4e58
Amos Langat <amos.langat@student.moringaschool.com>
2022-12-06 09:05:47 +0300

    modify seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,13 @@
 #   Character.create(name: "Luke", movie: movies.first)
 puts "creating airlines..."
 
-kq = Airline.create(name: "Kenya Airways", image_url: "", description: "Kenya Airways (KQ) is the National carrier in Kenya. Its main hub is at the Jomo Kenyatta International Airport (JKIA). The airline operates domestic flights to the following destinations within Kenya: Mombasa, Kisumu, Malindi, and Nairobi.")
-jambojet = Airline.create(name: "JamboJet", image_url: "", description: "Jambo jet is a low-cost airline and a subsidiary of Kenya Airways. It offers affordable domestic flights throughout Kenya. ")
-jetways = Airline.create(name: "Jetways", image_url: "", description: "We are a domestic flight company offering daily flights to Maasai Mara, Ukunda and Wajir from Wilson")
-mombasa_air_safari = Airline.create(name: "Mombasa Air Safari", image_url: "", description: "Mombasa Air Safari operates domestic flights from its hub at Moi International Airport in Mombasa. The airline specializes in direct flights between the coastal city and the best safari destinations in Kenya.")
-fly540= Airline.create(name: "Fly540", image_url: "", description: "Fly540 is a low-cost domestic airline, connecting major cities and tourist destinations in Kenya. The airline operates some of the best and most affordable flights in Kenya.")
-air_kenya = Airline.create(name: "AirKenya Airlines", image_url: "", description: "From its primary hub at Wilson Airport (WIL), AirKenya operates scheduled domestic flights within Kenya. Most of its journeys are to Kenya’s best tourist destinations.")
-safarilink = Airline.create(name: "Safarilink Aviation", image_url: "", description: "Safarilink is based at Nairobi’s Wilson Airport (WIL) and provides regional and domestic flights. They have daily services to many destinations within Kenya.")
+kq = Airline.create(name: "Kenya Airways", image_url: "https://i0.wp.com/www.busiweek.com/wp-content/uploads/2020/07/Kenyairways.jpg?fit=1000%2C600&ssl=1", description: "Kenya Airways (KQ) is the National carrier in Kenya. Its main hub is at the Jomo Kenyatta International Airport (JKIA). The airline operates domestic flights to the following destinations within Kenya: Mombasa, Kisumu, Malindi, and Nairobi.")
+jambojet = Airline.create(name: "JamboJet", image_url: "https://static.jambojet.com/media/1001/three-tails_v2.jpg?cropmode=percentaje&width=480", description: "Jambo jet is a low-cost airline and a subsidiary of Kenya Airways. It offers affordable domestic flights throughout Kenya. ")
+jetways = Airline.create(name: "Jetways", image_url: "https://jetwaysairlines.com/wp-content/uploads/2019/09/Fokker-100.jpg", description: "We are a domestic flight company offering daily flights to Maasai Mara, Ukunda and Wajir from Wilson")
+mombasa_air_safari = Airline.create(name: "Mombasa Air Safari", image_url: "https://storage.aerocrs.com/99/system/homebanner4.jpg", description: "Mombasa Air Safari operates domestic flights from its hub at Moi International Airport in Mombasa. The airline specializes in direct flights between the coastal city and the best safari destinations in Kenya.")
+fly540= Airline.create(name: "Fly540", image_url: "https://www.rotasturisticas.com/imagens/sectores/ke/large/SEC-6954-19170-fly_540_airlines.jpg", description: "Fly540 is a low-cost domestic airline, connecting major cities and tourist destinations in Kenya. The airline operates some of the best and most affordable flights in Kenya.")
+air_kenya = Airline.create(name: "AirKenya Airlines", image_url: "https://upload.wikimedia.org/wikipedia/en/thumb/d/d4/Airkenya_Express_logo.svg/1200px-Airkenya_Express_logo.svg.png", description: "From its primary hub at Wilson Airport (WIL), AirKenya operates scheduled domestic flights within Kenya. Most of its journeys are to Kenya’s best tourist destinations.")
+safarilink = Airline.create(name: "Safarilink Aviation", image_url: "https://als.co.ke/wp-content/uploads/2016/08/ALS-safarilink.png", description: "Safarilink is based at Nairobi’s Wilson Airport (WIL) and provides regional and domestic flights. They have daily services to many destinations within Kenya.")
 
 puts "creating users..."
 

aa2ae510b09ffe8e0c4189466089bd864781c5f4
Amos Langat <amos.langat@student.moringaschool.com>
2022-12-08 05:03:23 +0300

    add backend routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  resources :reviews, only: [:index, :show, :create, :update, :destroy]
+  resources :users, only: [:index, :destroy]
+  resources :airlines, param: :id, only: [:index, :show, :create, :update, :destroy]
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

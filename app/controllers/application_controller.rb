783179fbc63228cee1665c0fb888fa673461a600
Amos Langat <amos.langat@student.moringaschool.com>
2022-12-08 04:57:50 +0300

    add access control permission code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+    before_action :set_cors
+
+    def set_cors
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Request-Method'] = '*'
+    end
 end

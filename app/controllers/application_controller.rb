6f94d81cd8c6bc945d197efe050262153c32a57d
Amos Langat <amos.langat@student.moringaschool.com>
2022-12-08 10:36:11 +0300

    remove cors-policy code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
-    before_action :set_cors
-
-    def set_cors
-        headers['Access-Control-Allow-Origin'] = '*'
-        headers['Access-Control-Request-Method'] = '*'
-    end
 end
